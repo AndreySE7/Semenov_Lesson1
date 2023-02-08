20cf15e4d32a8234864823758f0c5b4090e1b8a2
a.semenov <drey_ac@mail.ru>
2023-02-08 22:33:40 +0300

    2nd commit

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,2 +1,16 @@
+data class Person(var name: String, var age: Int = 0, var city: String = "", var sex: String = "")
 fun main() {
+    val andrey = Person("Andrey").apply {
+        age = 28
+        city = "Saint-Petersburg"
+        sex = "men"
+    }
+    val yana = Person("Yana").apply {
+        age = 28
+        city = "Saint-Petersburg"
+        sex = "women"
+    }
+    println(andrey)
+    println(yana)
+
 }
